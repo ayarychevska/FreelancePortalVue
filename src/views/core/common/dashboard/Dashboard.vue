3f20a9230c658d4090275b6124adb0c5294ce117
Alevtyna Yarychevska <ayarychevska@gmail.com>
2021-02-14 16:06:40 +0100

    User view + dashboard changed

--- src/views/core/common/dashboard/Dashboard.vue
+++ src/views/core/common/dashboard/Dashboard.vue
@@ -1,13 +1,126 @@
 <template>
-    <h1> Dashboard </h1>
+    <div class="mx-5">
+        <h1 class="mt-3"> Welcome to Study platform </h1>
+
+        <div class="row">
+            <div class="col-md-10 pr-5">
+                <div class="row mt-4">
+                    <div
+                        class="col-lg-4 col-md-6"
+                        v-for="post in myPosts"
+                        :key="post.id"
+                    >
+                        <post-preview
+                            class="mb-3"
+                            :post="post"
+                            :editable="false"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <img
+                    src="@/assets/images/avatar.jpg"
+                    class="avatar uk-center uk-border-circle"
+                >
+                <h2 class="mt-3">{{ $store.state.auth.user.userName }}</h2>
+                <hr>
+                <div class="uk-text-center">
+                    <vk-button-link
+                        class="my-3"
+                        @click="$router.push({ name: 'user-view', params: { id: $store.state.auth.user.id }})"
+                        type="text"
+                    >
+                        <vk-icon
+                            icon="user"
+                            class="mr-2"
+                        ></vk-icon>Profile
+                    </vk-button-link>
+                    <br>
+                    <vk-button-link
+                        class="my-3"
+                        type="text"
+                        @click="$router.push({ name: 'my-posts'})"
+                    >
+                        <vk-icon
+                            icon="thumbnails"
+                            class="mr-2"
+                        ></vk-icon>My posts
+                    </vk-button-link>
+                    <br>
+                    <vk-button-link
+                        class="my-3"
+                        type="text"
+                        @click="$router.push({ name: 'calendar'})"
+                    >
+                        <vk-icon
+                            icon="calendar"
+                            class="mr-2"
+                        ></vk-icon>Calendar
+                    </vk-button-link>
+                    <br>
+                    <vk-button-link
+                        class="my-3"
+                        type="text"
+                        @click="$router.push({ name: 'appointment'})"
+                    >
+                        <vk-icon
+                            icon="clock"
+                            class="mr-2"
+                        ></vk-icon>Appointments
+                    </vk-button-link>
+                    <br>
+                    <vk-button-link
+                        class="my-3"
+                        type="text"
+                    >
+                        <vk-icon
+                            icon="commenting"
+                            class="mr-2"
+                        ></vk-icon>Messages
+                    </vk-button-link>
+                    <br>
+                    <vk-button-link
+                        class="my-3"
+                        @click="$router.push({ name: 'user-profile'})"
+                        type="text"
+                    >
+                        <vk-icon
+                            icon="cog"
+                            class="mr-2"
+                        ></vk-icon>Settings
+                    </vk-button-link>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
+import { PostListViewModel } from "@/models/core/teacher/Posts";
 import { Component, Vue } from "vue-property-decorator";
+import PostPreview from '@/views/core/common/post/Preview.vue';
+import { DateTime } from "luxon";
 
-@Component({})
+@Component({ components: { PostPreview } })
 export default class Dashboard extends Vue {
+    private myPosts: PostListViewModel[] = [];
 
+    async created(): Promise<void> {
+        const response = await this.$axios.get("/posts/list");
+        this.myPosts = response.data;
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+.separator {
+    border-left: solid 1px lightgray;
+}
+
+.avatar {
+    width: 60%;
+}
+</style>
 
---
+++ src/views/core/common/user/View.vue
@@ -0,0 +1,179 @@
+<template>
+    <div class="container">
+        <vk-card
+            class="uk-card-hover"
+            padding="small"
+        >
+            <div class="row">
+                <div class="col-5">
+                    <img
+                        src="@/assets/images/avatar.jpg"
+                        class="avatar uk-center uk-border-circle"
+                    >
+                    <h2 class="mt-3">{{ user.name }}</h2>
+                    <hr>
+
+                    <div class="uk-text-left ml-5 info">
+                        <div class="text-capitalize mb-2">
+                            <vk-icon
+                                icon="social"
+                                class="mr-2"
+                            ></vk-icon> {{ user.userType }}
+                        </div>
+                        <div
+                            type="email"
+                            class="mb-2"
+                        >
+                            <vk-icon
+                                icon="mail"
+                                class="mr-2"
+                            ></vk-icon> {{ user.email }}
+                        </div>
+                        <div class="mb-2">
+                            <vk-icon
+                                icon="calendar"
+                                class="mr-2"
+                            ></vk-icon> {{ prettyDateOfBirthDate }}
+                        </div>
+                        <div class="text-capitalize mb-2">
+                            <vk-icon
+                                icon="user"
+                                class="mr-2"
+                            ></vk-icon> {{ user.gender }}
+                        </div>
+                        <span style="font-size: 12px"><b> Subjects:</b></span><br>
+                        <div>
+                            {{ prettyListOfUserSubjects }}
+                        </div>
+                        <span style="font-size: 12px"><b> About me:</b></span><br>
+                        <span v-html="user.description"></span>
+
+                        <vk-button
+                            v-if="$store.state.auth.user.userType == 'teacher' && $store.state.auth.user.id != userId"
+                            class="uk-width-1-1 uk-button-secondary mt-3"
+                            @click="addAppointment(userId, user.name)"
+                        >Make an appointment with user</vk-button>
+                    </div>
+                </div>
+
+                <div class="col-7">
+                    <create-review
+                        v-if="userId != $store.state.auth.user.id"
+                        class="mb-3"
+                        :reviewingUserId='userId'
+                        :editable="false"
+                        @update-reviews="loadReviews()"
+                    />
+
+                    <template v-if="reviews.length > 0">
+                        <h3>Reviews</h3>
+                        <div
+                            v-for="review in reviews"
+                            :key="review.id"
+                        >
+                            <review-preview
+                                class="mb-3"
+                                :review="review"
+                                :editable="false"
+                            />
+                        </div>
+                    </template>
+                    <p v-else> No reviews yet </p>
+                </div>
+            </div>
+        </vk-card>
+    </div>
+</template>
+
+<script lang="ts">
+import { UserViewModel } from "@/models/core/common/Users";
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { DateTime } from "luxon";
+import ReviewPreview from "@/views/core/common/review/View.vue"
+import CreateReview from "@/views/core/common/review/Create.vue"
+import { ReviewViewModel } from "@/models/core/common/Reviews";
+
+@Component({ components: { ReviewPreview, CreateReview } })
+export default class UserView extends Vue {
+    private user: UserViewModel = {
+        name: "",
+        login: "",
+        email: "",
+        userType: "",
+        dateOfBirth: null,
+        gender: "",
+        description: "",
+        subjects: []
+    }
+
+    @Watch('userId')
+    private async reloadData(): Promise<void> {
+        await this.loadData();
+    }
+
+    private reviews: ReviewViewModel[] = []
+
+    private get prettyListOfUserSubjects(): string {
+        return this.capitalizeFirstLetter(this.user.subjects.map(x => x.title).join(', '));
+    }
+
+    private get userId(): string {
+        return this.$route.params.id || "";
+    }
+
+    async created(): Promise<void> {
+        await this.loadData();
+
+    }
+
+    async loadData(): Promise<void> {
+        const response: any = await this.$axios.get("/application-users/" + this.userId);
+        this.user = response.data;
+
+        await this.loadReviews();
+    }
+
+    async loadReviews() {
+        const loadReviews: any = await this.$axios.get("/reviews/user-reviews/" + this.userId);
+        this.reviews = loadReviews.data;
+    }
+
+
+    get prettyDateOfBirthDate() {
+        if (this.user.dateOfBirth)
+            return DateTime.fromISO(this.user.dateOfBirth.toString()).toLocaleString(DateTime.DATE_FULL);
+        else
+            return '-';
+    }
+
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    async addAppointment(studentId): Promise<void> {
+        this.$router.push({ name: 'appointment-add', params: { studentId: this.userId, studentName: this.user.name } })
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.display-flex-equal-pieces {
+    display: flex;
+    flex: 1 100%;
+    div {
+        flex: 1;
+    }
+}
+
+.avatar {
+    width: 30%;
+}
+
+.info {
+    font-size: 18px;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+        "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+        "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+</style>
